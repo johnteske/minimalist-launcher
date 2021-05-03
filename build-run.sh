f366c4283eba3bf12a5713946df7928d93d938df
John Teske <john.teske@protonmail.com>
2021-05-02 19:58:35 -0700

    Add reminder to turn on USB debugging

--- build-run.sh
+++ build-run.sh
@@ -2,6 +2,8 @@
 #
 # https://gist.github.com/aldemirenes/9bbea36abba33d74196ca5e488c4365e
 
+# double-check USB-debugging is set to "on" on the phone
+
 #package_name=launcher.minimalist.com
 package_name=launcher.simple.com.simplelauncher
 
